b173da554eec401b7e1cd4d5581acdb10858bfb6
Reddyong <d0209hong@naver.com>
2024-10-29 15:06:54 +0900

    feat : BOJ 9663 번 N- Queen -> 열, 왼쪽 대각선, 오른쪽 대각선에 대해 visited 를 각각 선언 -> 퀸을 해당 자리에 놓을 수 있는지를 이 3가지 visited 를 사용해 판단 후 백트래킹

--- src/main/java/boj/백트래킹/NQueen_9663/Main.java
+++ src/main/java/boj/백트래킹/NQueen_9663/Main.java
@@ -7,88 +7,45 @@ import java.io.InputStreamReader;
 public class Main {
     static int N;
     static int ans;
-    static boolean[][] visited;
+    static boolean[] visited1;
+    static boolean[] visited2;
+    static boolean[] visited3;
     public static void main(String[] args) throws IOException {
         init();
         solve();
     }
 
     private static void solve() {
-        for (int i = 0; i < N; i++) {
-            visited[0][i] = true;
-            recur(0, i, 1);
-            visited[0][i] = false;
-        }
+        recur(0);
 
         System.out.println(ans);
     }
 
-    private static void recur(int r, int c, int depth) {
+    private static void recur(int depth) {
         if (depth == N) {
             ans++;
             return;
         }
 
         for (int i = 0; i < N; i++) {
-            if (isPossibleQueen(r + 1, i)) {
-                visited[r + 1][i] = true;
-                recur(r + 1, i, depth + 1);
-                visited[r + 1][i] = false;
+            if (!visited1[i] && !visited2[depth + i] && !visited3[depth - i + N - 1]) {
+                visited1[i] = true;
+                visited2[depth + i] = true;
+                visited3[depth - i + N - 1] = true;
+                recur(depth + 1);
+                visited1[i] = false;
+                visited2[depth + i] = false;
+                visited3[depth - i + N - 1] = false;
             }
         }
-    }
-
-    private static boolean isPossibleQueen(int r, int c) {
-        // 위 직선
-        int nur = r;
-        int nuc = c;
-        while (isInBoard(nur, nuc)) {
-            if (visited[nur][nuc]) {
-                return false;
-            }
-
-            nur--;
-        }
-
-        // 왼쪽 위 대각선
-        int nlr = r;
-        int nlc = c;
-        while (isInBoard(nlr, nlc)) {
-            if (visited[nlr][nlc]) {
-                return false;
-            }
-
-            nlr--;
-            nlc--;
-        }
-
-        // 오른쪽 위 대각선
-        int nrr = r;
-        int nrc = c;
-        while (isInBoard(nrr, nrc)) {
-            if (visited[nrr][nrc]) {
-                return false;
-            }
-
-            nrr--;
-            nrc++;
-        }
-
-        return true;
-    }
-
-    private static boolean isInBoard(int r, int c) {
-        if (r < 0 || c < 0 || r >= N || c >= N) {
-            return false;
-        }
-
-        return true;
     }
 
     private static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        visited = new boolean[N][N];
+        visited1 = new boolean[N];
+        visited2 = new boolean[N * 2];
+        visited3 = new boolean[N * 2];
         ans = 0;
     }
 }
