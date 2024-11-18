31d692a8ad5fa939cbb73b41222db4efda1ef212
Reddyong <d0209hong@naver.com>
2024-11-18 17:32:03 +0900

    feat : BOJ 2410번 평범한 배낭 문제 -> dp 알고리즘을 이용해 풀기위해 패키지 변경

--- src/main/java/boj/dp/평범한배낭_12865/Main.java
+++ src/main/java/boj/dp/평범한배낭_12865/Main.java
@@ -1,4 +1,4 @@
-package boj.그리디.평범한배낭_12865;
+package boj.dp.평범한배낭_12865;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,37 +10,20 @@ public class Main {
     static int K;
     static int ans;
     static int[][] arr;
-    static boolean[] visited;
+    static int[][] dp;
     public static void main(String[] args) throws IOException {
         init();
         solve();
     }
 
     private static void solve() {
-        recur(0, 0, 0);
+        for (int i = 0; i < N; i++) {
+
+        }
 
         System.out.println(ans);
     }
 
-    private static void recur(int idx, int cur, int weight) {
-        if (weight > K) {
-            ans = Math.max(ans, cur - arr[idx - 1][1]);
-            return;
-        }
-
-        if (idx == N) {
-            ans = Math.max(ans, cur);
-            return;
-        }
-
-        for (int i = idx; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                recur(i + 1, cur + arr[i][1], weight + arr[i][0]);
-                visited[i] = false;
-            }
-        }
-    }
 
     private static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -49,7 +32,7 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         arr = new int[N][2];
-        visited = new boolean[N];
+        dp = new int[N][N];
         ans = 0;
 
         for (int i = 0; i < N; i++) {
