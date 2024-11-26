cfb62368103fcc2d815ad1a82233b6449d7c2e90
Reddyong <d0209hong@naver.com>
2024-11-26 16:33:55 +0900

    feat : BOJ 1978번 소수 찾기 문제 -> 소수 찾기 판별법을 이용하여 푸는 문제 -> i * i <= n 일때 까지 반복문을 돌며 나누어떨어지는 약수가 존재하는지 판별

---
+++ src/main/java/boj/수학/소수찾기_1978/Main.java
@@ -0,0 +1,54 @@
+package boj.수학.소수찾기_1978;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] nums;
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+    }
+
+    private static void solve() {
+        int ans = 0;
+
+        for (int num : nums) {
+            if (isPrimeNumber(num)) {
+                ans++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    private static boolean isPrimeNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
