cf84223bf47e1bdb414cfa04729619fe5f7cc14b
Reddyong <d0209hong@naver.com>
2025-02-04 14:12:20 +0900

    feat : programmers Lv.2 캐시 문제 -> 큐를 사용하여 문제에 주어진 조건에 맞게 LRU 알고리즘을 구현하여 푸는 문제

---
+++ src/main/java/programmers/Lv2/캐시/Solution.java
@@ -0,0 +1,66 @@
+package programmers.Lv2.캐시;
+
+import java.util.*;
+
+public class Solution {
+    int answer;
+    Queue<String> queue;
+    public int solution(int cacheSize, String[] cities) {
+        init();
+        solve(cacheSize, cities);
+
+        return answer;
+    }
+
+    private void solve(int cacheSize, String[] cities){
+        if(cacheSize == 0){
+            answer = 5 * cities.length;
+            return;
+        }
+
+        for(String city : cities){
+            city = city.toLowerCase();
+
+            if(queue.isEmpty()){
+                queue.add(city);
+                answer += 5;
+                continue;
+            }
+
+            if(queue.contains(city)){
+                answer += 1;
+                queue.remove(city);
+            } else{
+                answer += 5;
+            }
+
+            if(queue.size() == cacheSize){
+                queue.poll();
+            }
+
+            queue.add(city);
+
+        }
+    }
+
+    private void init(){
+        answer = 0;
+        queue = new LinkedList<>();
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int solution1 = sol.solution(3, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"});
+        int solution2 = sol.solution(3, new String[]{"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"});
+        int solution3 = sol.solution(2, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"});
+        int solution4 = sol.solution(5, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"});
+        int solution5 = sol.solution(2, new String[]{"Jeju", "Pangyo", "NewYork", "newyork"});
+        int solution6 = sol.solution(0, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork", "LA"});
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+        System.out.println("solution3 = " + solution3);
+        System.out.println("solution4 = " + solution4);
+        System.out.println("solution5 = " + solution5);
+        System.out.println("solution6 = " + solution6);
+    }
+}
