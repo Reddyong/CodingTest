b3ea1172147c8389bf8bf5ba95a981e5821f46a5
Reddyong <d0209hong@naver.com>
2025-04-07 12:17:30 +0900

    feat : BOJ 13458번 시험 감독 문제 -> 구현문제이지만, 정수의 크기를 잘 생각하여야 한다..

---
+++ src/main/java/boj/시험감독_13458/Main.java
@@ -0,0 +1,60 @@
+package boj.시험감독_13458;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int N;
+    private static int[] A;
+    private static int B;
+    private static int C;
+    private static long answer;
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+    }
+
+    private static void solve() {
+        for(int i = 0; i < N; i++){
+            if (A[i] <= B) {
+                A[i] = 0;
+            } else {
+                A[i] -= B;
+            }
+
+            answer++;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (A[i] % C != 0) {
+                answer += A[i] / C + 1;
+            } else {
+                answer += A[i] / C;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        A = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        answer = 0;
+    }
+}
