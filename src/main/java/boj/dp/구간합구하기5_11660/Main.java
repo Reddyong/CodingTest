1eac00755d5c13a8bc6c3adfb46bd6cbfaf8eac3
Reddyong <d0209hong@naver.com>
2025-05-07 12:06:23 +0900

    feat : BOJ 11660번 구간 합 구하기 5 문제 -> 2차원 dp 배열을 사용하는 구간합 구하기 문제

---
+++ src/main/java/boj/dp/구간합구하기5_11660/Main.java
@@ -0,0 +1,61 @@
+package boj.dp.구간합구하기5_11660;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int N, M;
+    private static int[][] board, dp, range;
+    public static void main(String[] args) throws IOException {
+        // 초기화
+        init();
+        // 풀이 과정
+        solve();
+    }
+
+    private static void solve() {
+        // dp 배열 채우기
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + board[i][j];
+            }
+        }
+
+        // 주어진 범위의 합 출력하기
+        for (int[] r : range) {
+            int x1 = r[0];
+            int y1 = r[1];
+            int x2 = r[2];
+            int y2 = r[3];
+
+            System.out.println(dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1]);
+        }
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        board = new int[N + 1][N + 1];
+        dp = new int[N + 1][N + 1];
+        range = new int[M][4];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 4; j++) {
+                range[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+}
