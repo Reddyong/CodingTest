872d419ec9f19f8b01b34c940c3ee6b14b7ed3f8
Reddyong <d0209hong@naver.com>
2025-05-17 14:30:42 +0900

    feat : BOJ 2565번 전깃줄 문제 -> LIS 알고리즘을 dp를 사용하여 O(N^2)에 푸는 문제 -> N이 여유로운 수일때는 dp를 사용해도 된다

---
+++ src/main/java/boj/dp/전깃줄_2565/Main.java
@@ -0,0 +1,56 @@
+package boj.dp.전깃줄_2565;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int N, answer;
+    private static int[][] line;
+    private static int[] dp;
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+    }
+
+    private static void solve() {
+        Arrays.sort(line, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            }
+
+            return o1[0] - o2[0];
+        });
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (line[i][0] > line[j][0] && line[i][1] > line[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(N - answer);
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        line = new int[N][2];
+        dp = new int[N];
+        answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            line[i][0] = Integer.parseInt(st.nextToken());
+            line[i][1] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
